44df91fd214c30fab9fd10fd35878d3a357c49a9
koukic <ck227200@gmail.com>
2019-06-08 13:45:58 +0000

    image-tag

---
+++ app/models/information.rb
@@ -0,0 +1,22 @@
+class Information < ApplicationRecord
+  before_destroy :not_referenced_by_any_line_item
+  mount_uploader :image, ImageUploader
+  serialize :image, JSON
+  has_many :line_items
+  belongs_to :user, optional: true
+  validates :title, presence: true
+  validates :description, length: { maximum: 1000, too_long: "%{count} characters is the maximum
+  allowed."}
+  validates :title, length: { maximum: 140, too_long: "%{count} characters is the maximum
+  allowed."}
+  CONDITION = %w{ New Fair Poor }
+  
+  private
+  
+  def not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, "Line items present")
+      throw :abort
+    end
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :line_items
+  resources :carts
+  resources :information
+  devise_for :users, controllers: {
+    registrations: 'registrations'
+  }
+  
+  
+  root 'information#index'
+end
