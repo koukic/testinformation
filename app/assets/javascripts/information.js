a28ef293535308f1c2501a5f5636038ac501d1db
koukic <ck227200@gmail.com>
2019-06-25 09:47:17 +0000

    some image

--- app/assets/javascripts/information.js
+++ app/assets/javascripts/information.js
@@ -1,56 +1,17 @@
-document.addEventListener("turbolinks:load", function() {
-
-  var instrumentImage = document.querySelector('.instrument-image');
-
-  function handleFileSelect(evt) {
-    var files = evt.target.files; // FileList object
-
-    // Loop through the FileList and render image files as thumbnails.
-    for (var i = 0, f; f = files[i]; i++) {
-
-      // Only process image files.
-      if (!f.type.match('image.*')) {
-        continue;
-      }
-
-      var reader = new FileReader();
-
-      // Closure to capture the file information.
-      reader.onload = (function(theFile) {
-        return function(e) {
-          // Render thumbnail.
-          var span = document.createElement('span');
-          span.innerHTML = ['<img class="instrument-preview-thumb" src="', e.target.result,
-            '" title="', escape(theFile.name), '"/>'
-          ].join('');
-          document.getElementById('list').insertBefore(span, null);
-        };
-      })(f);
-      // Read in the image file as a data URL.
-      reader.readAsDataURL(f);
-    }
-  }
-
-  if (instrumentImage) {
-    this.addEventListener('change', handleFileSelect, false);
-  }
-
-});
-
-
-
 $(function(){
     $('#information_image').change(function(){
         $('img').remove();
-        var file = $(this).prop('files')[0];
-        if(!file.type.match('image.*')){
+        var file = $(this).prop('files');
+        $(file).each(function(i) {
+        if(!file[i].type.match('image.*')){
             return;
         }
         var fileReader = new FileReader();
         fileReader.onloadend = function() {
-            $('#result').html('<img src="' + fileReader.result + '"/>');
+            $('#result').append('<img src="' + fileReader.result + '"/>');
         }
-        fileReader.readAsDataURL(file);
+        fileReader.readAsDataURL(file[i]);
+        });
     });
 });
 
